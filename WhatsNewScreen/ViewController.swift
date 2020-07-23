5f1021c3e046af67db91a3c29d285f27c230482c
yusuke mitsugi <pastabal.jungle@gmail.com>
2020-07-23 16:47:36 +0900

    Initial Commit

---
+++ WhatsNewScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsNewScreen
+//
+//  Created by Yusuke Mitsugi on 2020/07/23.
+//  Copyright © 2020 Yusuke Mitsugi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
